587c42cd13aa0912b77678b5cfa0de055b358b31
Justis Faulkner <faulknerjustis@gmail.com>
2024-03-26 18:10:40 -0400

    solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  // (32°F − 32) × 5/9
+  return Math.round(((degreesF - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  // (0°C × 9/5) + 32
+  return Math.round(((degreesC * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
