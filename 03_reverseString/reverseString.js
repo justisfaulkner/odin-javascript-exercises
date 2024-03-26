854c09ad8fb6928132ff5326fecbc853ba14016b
Justis Faulkner <faulknerjustis@gmail.com>
2024-03-26 11:52:49 -0400

    solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let finalString = "";
 
+    for (i = string.length; i > 0; i--) {
+        finalString += string[i - 1];
+    }
+
+    return finalString;
 };
+
+// console.log(reverseString("Hello, World!"))
 
 // Do not edit below this line
 module.exports = reverseString;
