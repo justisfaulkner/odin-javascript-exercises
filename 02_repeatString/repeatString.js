e2e5089ed72a9487260a0e9771d26db24c6585ac
Justis Faulkner <faulknerjustis@gmail.com>
2024-03-26 11:37:49 -0400

    solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let finalString = "";
 
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < num; i++) {
+        finalString += string;
+    }
+
+    return finalString;
 };
+
+console.log(repeatString('hey', -3))
 
 // Do not edit below this line
 module.exports = repeatString;
