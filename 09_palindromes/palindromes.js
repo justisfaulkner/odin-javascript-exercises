f8d55b5f27807415f6107023a237d8ae9c11cb8f
Justis Faulkner <faulknerjustis@gmail.com>
2024-05-05 17:56:25 -0400

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const formattedString = string.replace(/\W+/g, '').toLowerCase();
+    let backwards = '';
+    for (let i = formattedString.length - 1; i >= 0; i--) {
+        backwards += formattedString[i];
+    }
+    return formattedString === backwards
 };
 
 // Do not edit below this line
