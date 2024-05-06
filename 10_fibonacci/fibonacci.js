b512ad264bac533471b595c3a0cbcf7d05bba412
Justis Faulkner <faulknerjustis@gmail.com>
2024-05-05 22:27:43 -0400

    solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) return "OOPS"
+    
+    let nMinus2 = 0;
+    let nMinus1 = 0;
+    let nCurr = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i === 1 || i === 2) {
+            nMinus1 = 1;
+            nMinus2 = 1;
+            nCurr = 1;
+            continue;
+        }
+        nCurr = nMinus2 + nMinus1;
+        nMinus2 = nMinus1
+        nMinus1 = nCurr
+    }
+    
+    return nCurr;
+}
 
-};
+console.log(fibonacci("cat"))
 
 // Do not edit below this line
 module.exports = fibonacci;
